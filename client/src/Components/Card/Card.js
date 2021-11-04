4b5be2b8959579331cf749db3ecc8f240daf293f
Alfredo <alfredocalvelo1@gmail.com>
2021-11-04 00:20:07 -0300

    Estrellas en rating

--- client/src/Components/Card/Card.js
+++ client/src/Components/Card/Card.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from './Card.module.css'
+import { FaStar, FaStarHalf } from "react-icons/fa";
 
 function Card (props) {
-  console.log(props);
+  // const [stars, setStars]=useState('pendig')
+  const [stars,setStars] = useState()
+
+  useEffect(()=>{
+    let rating = props.res.rating
+    let rest = rating-parseInt(rating)
+    let arrReturn =[]
+    for (let i = 0; i < parseInt(rating); i++) {
+      arrReturn.push(<FaStar/>)
+    }
+    if(rest>0.75){
+      arrReturn.push(<FaStar/>)
+    }else if(rest>0.25 && rest<=0.75){
+      arrReturn.push(<FaStarHalf/>)
+    }
+    setStars(arrReturn)
+  })
 
   return(
     <div className = {styles.Card} >
-      <div>
-        {props.res.name}
+      <div className = {styles.Izquierda}>
+        <div className={styles.Nombre}>
+          {props.res.name}
+        </div>
+        <div className = {styles.Stars}>
+          {stars}
+        </div>
+        <div>
+          {props.res.genres}
+        </div>
+        <div>
+          {props.res.platforms}
+        </div>
+        <div>
+          {props.res.released}
+        </div>
       </div>
-      <div>
-        <img className={styles.img} src = {props.res.background_image}/>
-      </div>
-      <div>
-        {props.res.rating}
-      </div>
-      <div>
-        {props.res.genres}
-      </div>
-      <div>
-        {props.res.platforms}
-      </div>
-      <div>
-        {props.res.released}
+      <div className = {styles.Derecha}>
+        <div>
+          <img className={styles.img} src = {props.res.background_image}/>
+        </div>
       </div>
     </div>
   )
--- client/src/Components/GameList/Gamelist.js
+++ client/src/Components/GameList/Gamelist.js
@@ -7,26 +7,53 @@ const { default:axios } = require('axios')
 function GameList() {
 
   const[res,setRes]=useState('pending')
+  const [page, setPage] = useState(1)
+  const [index, setIndex] = useState([0,14])
 
-  // (async function () {
-  //   let res = await axios('http://localhost:3001/buscar')
-  //   setRes(res)
-  // })()
   useEffect(async()=>{
     if(res === 'pending'){
       let res = await axios('http://localhost:3001/buscar')
       setRes(res.data)
     }
   })
-  
-  
-  
+  function onClickUp(){
+    let length = res.length-1
+    if(index[1]=== 104){
+    }else{
+      setPage(page+1)
+      setIndex([index[0]+15,index[1]+15])
+    }
+  }
+  function onClickDown(){
+    
+    if(index[0]=== 0){
+    }else{
+      setPage(page-1)
+      setIndex([index[0]-15,index[1]-15])
+
+    } 
+
+  }
 
   return(
   <div className = {styles.Container}>
       {res === 'pending'? <div className = {styles.Loader}></div>:  
           <>
-          {res.map((elem)=>{return <Card res = {elem}/>})}
+          {res.map((elem)=>{
+            if(res.indexOf(elem) >= index[0] && res.indexOf(elem)<= index[1]){
+              return <Card res = {elem}/>
+            }else{return null}
+          })}
+          <span> {page} </span>
+          <button onClick = {()=>{
+            onClickUp()
+            window.scrollTo(0,0)
+          }
+            }>siguiente</button>
+          <button onClick = {()=>{
+            window.scrollTo(0,0)
+            onClickDown()
+            }}>anterior</button>
           </>
       }
   </div>
